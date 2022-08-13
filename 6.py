75c73e0d2b3e69cd704bae1ab3dd6e95715ff3e7
fatemehnejatifar <72495993+fatemehnejatifar@users.noreply.github.com>
2022-08-13 08:11:00 -0700

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,7 @@
+a=int(input('addadi sheshraghami ra vared konid= ')) 
+raghamedovom=((a//10)%10) 
+raghamepanjom=((a//10000)%10) 
+majmu=raghamedovom+raghamepanjom 
+print('majmoe raghame dovom va panjom =' , majmu)
+
+
