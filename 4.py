9900b7ed41f6ef9fa991c03afbc028ed5fabb553
fatemehnejatifar <72495993+fatemehnejatifar@users.noreply.github.com>
2022-09-01 03:43:16 -0700

    Update and rename 4.py to 4.triangle.py

--- 4.py
+++
@@ -1,4 +0,0 @@
-a=float(input(' ghaede ra vared konid :'))
-h=float(input('mehgdare ertefa ra vared konid : '))
-masahat=((a*h)/2)
-print('masahat =', masahat)
---
+++ 4.triangle.py
@@ -0,0 +1,6 @@
+ghaedeh=float(input('  meghdare ghaede ra vared konid :'))
+ertefa=float(input('mehgdare ertefa ra vared konid : '))
+masahat=((ghaedeh*ertefa)/2)
+print('masahat =', masahat)
+
+
