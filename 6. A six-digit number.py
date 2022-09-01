ba9d595b9be293c71785bfa90319b0f8e71c996e
fatemehnejatifar <72495993+fatemehnejatifar@users.noreply.github.com>
2022-09-01 03:46:23 -0700

    Update 6. A six-digit number.py

--- 6. A six-digit number.py
+++ 6. A six-digit number.py
@@ -1,6 +1,6 @@
-a=int(input('addadi sheshraghami ra vared konid= ')) 
-raghamedovom=((a//10)%10) 
-raghamepanjom=((a//10000)%10) 
+addad=int(input('addadi sheshraghami ra vared konid= ')) 
+raghamedovom=((addad//10)%10) 
+raghamepanjom=((addad//10000)%10) 
 majmu=raghamedovom+raghamepanjom 
 print('majmoe raghame dovom va panjom =' , majmu)
 
